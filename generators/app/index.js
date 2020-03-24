4f16923e318fcabacf92fe9b2f9cb59d6b90decc
Joseph Sinfield <josephsinfield@yahoo.com>
2020-03-24 23:13:25 +0000

    Compose dotfiles generator in to tdd generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,29 +1,33 @@
 const Generator = require('yeoman-generator');
+const DotfilesGenerator = require('/home/developer/yeoman-generators/generator-dotfiles/generators/app/index.js');
 
 const MyBase = class extends Generator {
   copyTemplateFiles() {
     // Copy all non-dotfiles
     this.fs.copy(
+      // If dotfiles are reintroduced
+      // the repeat this with '.*'
       this.templatePath('**/*'),
-      this.destinationRoot()
-    );
-
-    // Copy all dotfiles
-    this.fs.copy(
-      this.templatePath('.*'),
       this.destinationRoot()
     );
   };
 };
 
 module.exports = class extends MyBase {
+  initializing() {
+    this.composeWith({
+      Generator: DotfilesGenerator,
+      path: require.resolve('/home/developer/yeoman-generators/generator-dotfiles/generators/app')
+    });
+  };
+
   writing() {
     this.copyTemplateFiles();
     this.config.save();
-  }
+  };
 
   install() {
     this.npmInstall();
-  }
+  };
 };
 
